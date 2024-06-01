33e56b2cce103e2c86bcd8e2df0e2a9c4a8b74e0
@kamanaft <hadzage@gmail.com>
2024-06-01 20:09:57 +0200

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,14 +2,14 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
-        boolean result = true;
+        boolean result = false;
+        int flag = 0;
         for (int i = 0; i < board[row].length; i++) {
-            int flag = 0;
             if (board[row][i] == 'X') {
                 flag += 1;
             }
             if (flag == board[row].length) {
-                result = false;
+                result = true;
                 break;
             }
         }
@@ -17,16 +17,16 @@ public class MatrixCheck {
     }
 
     public static boolean monoVertical(char[][] board, int column) {
-        boolean result = true;
+        boolean result = false;
+        int flag = 0;
         for (int i = 0; i < board.length; i++) {
-            int flag = 0;
             if (board[i][column] == 'X') {
                 flag += 1;
-                }
+            }
             if (flag == board.length) {
-                result = false;
+                result = true;
                 break;
-                }
+            }
         }
         return result;
     }
@@ -35,6 +35,17 @@ public class MatrixCheck {
         char[] result = new char[board.length];
         for (int i = 0; i < board.length; i++) {
             result[i] = board[i][i];
+        }
+        return result;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                result = true;
+                break;
+            }
         }
         return result;
     }
